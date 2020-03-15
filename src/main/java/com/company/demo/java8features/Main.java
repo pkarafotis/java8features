88fe27e6e678fbda1b5c0b34a1aabae8dad0ae8f
Panagiotis Karafotis <Panagiotis.KARAFOTIS@intrasoft-intl.com>
2020-03-15 19:52:44 +0200

    Optionals demo

--- src/main/java/com/company/demo/java8features/Main.java
+++ src/main/java/com/company/demo/java8features/Main.java
@@ -1,20 +1,16 @@
 package com.company.demo.java8features;
 
-import com.company.demo.java8features.defaultMethod.DefaultMethod;
-import com.company.demo.java8features.defaultMethod.DefaultMethodImpl;
+import com.company.demo.java8features.optionals.Optionals;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("*** START Default Methods ***");
+        System.out.println("*** START Optionals***");
 
-        DefaultMethod dm = new DefaultMethodImpl();
+        Optionals.checkOptionalsWithoutContent();
+        Optionals.checkOptionalsWithContent();
 
-        System.out.println("Method implementation on class sum(5,10): " + dm.add(5,10));
-
-        System.out.println("Default Method implementation on interface avg(5,10): " + dm.avg(5,10));
-
-        System.out.println("*** END Default Methods ***\n\n");
+        System.out.println("*** END Optionals***\n\n");
     }
 }
--- src/main/java/com/company/demo/java8features/defaultMethod/DefaultMethodImpl.java
+++
@@ -1,9 +0,0 @@
-package com.company.demo.java8features.defaultMethod;
-
-public class DefaultMethodImpl implements DefaultMethod {
-
-    @Override
-    public int add(int a, int b) {
-        return a+b;
-    }
-}
